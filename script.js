187abda18f797c38bb4fba6077f7640676c43d57
NykSchizo <107075864+schizophrenickkjk@users.noreply.github.com>
2024-09-19 09:06:14 -0300

    tete

--- script.js
+++ script.js
@@ -9,12 +9,10 @@ const perguntas = [
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Isso é assustador!"
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Isso é maravilhoso!"
             }
         ]
     },
