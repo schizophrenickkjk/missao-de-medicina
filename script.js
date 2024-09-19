8c63adbd95bac5e55709af27b3b6c684de263964
NykSchizo <107075864+schizophrenickkjk@users.noreply.github.com>
2024-09-19 09:09:20 -0300

    testeee

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Tendo em vista que a medicina no Paraguai é um assunto muito comentado no Brasil, diga-nos a sua opinião geral: você preferiria fazer medicina no Paraguai ou no Brasil?",
         alternativas: [
             {
-                texto: "Isso é assustador!"
+                texto: "No Paraguai."
             },
             {
-                texto: "Isso é maravilhoso!"
+                texto: "No Brasil."
             }
         ]
     },
